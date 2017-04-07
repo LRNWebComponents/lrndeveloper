1474a73f2ced43ab47a258bcb21e9d47dd8a23cf
btopro <btopro@outlook.com>
2017-04-07 15:15:18 -0400

    path abstracted from update-all

--- sync-down-github.sh
+++ sync-down-github.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # pull all repos
 
-cd ~/ && mkdir -p Documents/git/elmsln/LRNWebComponents/ && cd Documents/git/elmsln/LRNWebComponents/ && curl -s https://api.github.com/orgs/LRNWebComponents/repos?per_page=200 | ruby -rubygems -e 'require "json"; JSON.load(STDIN.read).each { |repo| %x[git clone \"#{repo["html_url"]}\" ]}' && cd lrndeveloper
+# where am i? move to where I am. This ensures source is properly sourced
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+# go back a level so we have all the repos here
+cd ../
+curl -s https://api.github.com/orgs/LRNWebComponents/repos?per_page=200 | ruby -rubygems -e 'require "json"; JSON.load(STDIN.read).each { |repo| %x[git clone \"#{repo["html_url"]}\" ]}' && cd lrndeveloper
 
 echo "All the webcomponent repos have been pulled down."
