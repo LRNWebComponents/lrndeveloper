e659943049fe44861aa4e79a2ac5f1abdf79d6c3
btopro <btopro@outlook.com>
2017-11-29 15:30:56 -0500

    updated for end result

--- lrn-build-dependency.sh
+++ lrn-build-dependency.sh
@@ -76,10 +76,9 @@ vulcanize index.html > build.html
 # move back below the repo
 cd ../../..
 # support for updating ELMSLN copy of this
-cp build/build/default/build.html ~/elmsln/core/dslmcode/shared/drupal-7.x/libraries/webcomponents/polymer/LRNWebcomponents/build.html
-cp build/build/default/push-manifest.json ~/elmsln/core/dslmcode/shared/drupal-7.x/libraries/webcomponents/polymer/LRNWebcomponents/manifest.json
-cp build/build/default/service-worker.js ~/elmsln/core/dslmcode/shared/drupal-7.x/libraries/webcomponents/polymer/LRNWebcomponents/service-worker.js
-
-cp build-for-edit/build/default/build.html ~/elmsln/core/dslmcode/shared/drupal-7.x/modules/elmsln_contrib/cis_connector/modules/features/elmsln_core/LRNWebComponents/build_edit.html
+cp build/build/default/build.html ~/elmsln/core/webcomponents/elements/_build/build.html
+cp build/build/default/push-manifest.json ~/elmsln/core/webcomponents/elements/_build/manifest.json
+cp build/build/default/service-worker.js ~/elmsln/core/webcomponents/elements/_build/service-worker.js
+cp build-for-edit/build/default/build.html ~/elmsln/core/webcomponents/elements/_build/build_edit.html
 
 echo "$repo as well as the LRN build file have been updated"
